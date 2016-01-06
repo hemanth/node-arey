3c1f55666e06215de1305714475e5dec4e53ca0d
Hemanth.HM <hemanth.hm@gmail.com>
2016-01-06 08:41:00 +0530

    after a year...

--- index-es6.js
+++ index-es6.js
@@ -1,14 +1,14 @@
+"use strict";
 export class Arey extends Array {
   constructor(size) {
     super(size);
   }
-}
-
-Arey.of = function() {
-    return [].slice.call(arguments);
-};
-
-Arey.isArray = function(arr) {
+  static of(...args) {
+    return this.slice.call(...args);
+  }
+  static isArray(arr) {
     // Array.isArray(arr) is false :/
-    return arr.constructor.name === 'Arey' || Object.prototype.toString.call(arr) === '[object Array]';
-};
+    return arr.constructor.name === 'Arey' ||
+    Object.prototype.toString.call(arr) === '[object Array]';
+  }
+}
