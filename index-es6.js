2f9ad25ac366e890cb623a6536d885fff1e4934e
Hemanth.HM <hemanth.hm@gmail.com>
2017-01-06 20:52:42 +0530

    After an other year...

--- index-es6.js
+++ index-es6.js
@@ -1,14 +1,19 @@
 "use strict";
-export class Arey extends Array {
+class Arey extends Array {
   constructor(size) {
     super(size);
   }
   static of(...args) {
-    return this.slice.call(...args);
+    return Array.prototype.slice.call(args);
   }
   static isArray(arr) {
     // Array.isArray(arr) is false :/
     return arr.constructor.name === 'Arey' ||
     Object.prototype.toString.call(arr) === '[object Array]';
   }
+  static diff(arr1, arr2) {
+    return arr1.filter(x => arr2.indexOf(x) == -1);
+  }
 }
+
+module.exports = Arey;
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
-, Arey = require('./index').Arey
+, Arey = require('./index')
 , list = new Arey();
 
 assert.equal(Arey.of(1,2,3).length,3);
 assert.equal(Arey.isArray([1,2,3]),true,"isArray failed");
+assert.deepEqual(Arey.diff([1,2,3],[3,4,5]),[1,2],"diff failed");
